be68941f111520632bc5b6906b8ad766ea0de1ae
LucasGalmeida <48827453+LucasGalmeida@users.noreply.github.com>
2019-10-30 00:10:24 -0300

    Adicionado algumas linhas de comentário
    Falta fazer o modo panico

--- AnalisadorSintatico.py
+++ AnalisadorSintatico.py
@@ -55,71 +55,67 @@ class Sintatico:
         self.lex = None
         self.tokenAtual = None
 
+    # Inicia o analisador sintatico
     def interprete(self, nomeArquivo):
-        if not self.lex is None:
-            print('ERRO: Já existe um arquivo sendo processado.')
-        else:
+        if not self.lex is None:  # Verifica se ja ha um arquivo sendo lido pelo lexico
+            print('ERRO: ja existe um arquivo sendo processado.')
+        else:  # Se nao houver, inicia a leitura
             self.lex = Lexico(nomeArquivo)
             self.lex.abreArquivo()
-            self.tokenAtual = self.lex.getToken()
+            self.tokenAtual = self.lex.getToken()  # Pede ao lexico para enviar o primeiro token
 
-            self.A()
-            # Talvez pode ser retirado
-            # self.consome(tt.FIMARQ)
+            self.A()  # Nao terminal inicial da gramatica
 
-            self.lex.fechaArquivo()
+            self.lex.fechaArquivo()  # Fecha o arquivo
 
-    def atualIgual(self, token):
+    def atualIgual(self, token):  # Verifica se o token lido é igual ao esperado
         (const, msg) = token
         return self.tokenAtual.const == const
 
-    def consome(self, token):
-        if self.atualIgual(token):
+    def consome(self, token):  # Consome o token atual, e chama o proximo
+        if self.atualIgual(token):  # Verifica se o token foi o esperado
             self.tokenAtual = self.lex.getToken()
-        else:
+        else:  # Se o token nao for o esperado
             (const, msg) = token
             print('ERRO DE SINTAXE [linha %d]: era esperado "%s" mas foi recebido "%s"'
                   % (self.tokenAtual.linha, msg, self.tokenAtual.lexema))
+
+            #self.tokenAtual = self.lex.getToken()
             quit()
 
-    def A(self):
-        self.PROG()
-        self.consome(tt.FIMARQ)
+    def A(self):  # Nao terminal Inicial
+        self.PROG()  # Chama o "Main"
+        self.consome(tt.FIMARQ)  # Consome o fim de arquivo
 
-    def PROG(self):
-        self.consome(tt.PROGRAMA)
-        self.consome(tt.ID)
-        self.consome(tt.PVIRG)
-        self.DECLS()
-        self.C_COMP()
+    def PROG(self):  # Nao terminal "Main"
+        self.consome(tt.PROGRAMA)  # Consome o terminal programa
+        self.consome(tt.ID)  # Consome o terminal que indica o "nome" do programa
+        self.consome(tt.PVIRG)  # Consome o terminal ponto e virgula
+        self.DECLS()  # Chama as declaracoes
+        self.C_COMP()  # Chama a funcao que abre chaves, chama os comandos e fecha chaves
 
-    def DECLS(self):
-        if self.atualIgual(tt.VARIAVEIS):
-            self.consome(tt.VARIAVEIS)
-            self.LIST_DECLS()
-        else:
+    def DECLS(self):  # Nao terminal das declaracoes
+        if self.atualIgual(tt.VARIAVEIS):  # Se houver alguma variavel nova
+            self.consome(tt.VARIAVEIS)  # Consome o terminal variaveis
+            self.LIST_DECLS()  # Chama o nao terminal responsavel pela estrutura das variaveis
+        else:  # Se nao houver mais nenhuma variavel
             pass
 
-    def LIST_DECLS(self):
-        self.DECL_TIPO()
-        self.D()
+    def LIST_DECLS(self):  # Nao terminal responsavel pela estrutura das variaveis
+        self.DECL_TIPO()  # Chama o nao terminal responsavel pela estrutura das variaveis
+        self.D()  # Chama o nao terminal que verifica se ha mais outra variavel
 
-    def D(self):
-        if self.atualIgual(tt.ID):
+    def D(self):  # Terminal que verifica se ha mais outra variavel
+        if self.atualIgual(tt.ID):  # Se tiver lido um ID, é outra variavel
             self.LIST_DECLS()
-        else:
+        else:  # Se nao, acabou
             pass
 
-        # if self.atualIgual(tt.FIMARQ):
-        #   pass
-        # else:
-        #   self.LIST_DECLS()
-
-    def DECL_TIPO(self):
-        self.LIST_ID()
-        self.consome(tt.DPONTOS)
-        self.TIPO()
-        self.consome(tt.PVIRG)
+    def DECL_TIPO(self):  # Nao terminal responsavel pela estrutura das variaveis
+        self.LIST_ID()  # Primeiro chama o nao terminal responsavel por ler um ID
+        self.consome(tt.DPONTOS)  # Depois consome o terminal DoisPontos
+        self.TIPO()  # Depois chama o nao terminal responsavel pelo tipo da variavel
+        self.consome(tt.PVIRG)  # Depois consome o terminal ponto e virgula
 
     def LIST_ID(self):
         self.consome(tt.ID)
@@ -154,15 +150,10 @@ class Sintatico:
     def G(self):
 
         if self.atualIgual(tt.SE) or self.atualIgual(tt.ENQUANTO) or self.atualIgual(tt.LEIA) or self.atualIgual(
-                tt.ESCREVA) or self.atualIgual(tt.ID):  ##########
+                tt.ESCREVA) or self.atualIgual(tt.ID):
             self.LISTA_COMANDOS()
         else:
             pass
-
-        # if self.atualIgual(tt.FIMARQ):
-        #    pass
-        # else:
-        #    self.LISTA_COMANDOS()
 
     def COMANDOS(self):
         if self.atualIgual(tt.SE):
@@ -173,7 +164,7 @@ class Sintatico:
             self.READ()
         elif self.atualIgual(tt.ESCREVA):
             self.WRITE()
-        elif self.atualIgual(tt.ID):  #######3
+        elif self.atualIgual(tt.ID):
             self.ATRIB()
 
     def IF(self):
@@ -287,7 +278,7 @@ class Sintatico:
 
 
 if __name__ == "__main__":
-    nome = "exemplo10.txt"
+    nome = "exemplo2.txt"
 
     parser = Sintatico()
     parser.interprete(nome)
